b7be6c250adb231aecdce8e3cd8a7d1bf8e4b9ec
Luis Rodriguez Luque <lluisrl@outlook.com>
2020-04-08 16:58:57 +0200

    Server ready

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const { check } = require('express-validator');
-
+const helmet = require('helmet');
 
 // Server constructor
 const app = express();
+
+app.use(helmet());
+
+app.get('/', (req, res) => {
+    res.send('Hola mundo')
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`)
+})
